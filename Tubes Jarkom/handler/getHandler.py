447ccf93b6bfcedb2a732bee526418daf81df008
luthfinovra <lutvinovra@gmail.com>
2023-05-06 14:14:10 +0700

    Add Comment

--- Tubes Jarkom/handler/getHandler.py
+++ Tubes Jarkom/handler/getHandler.py
@@ -1,15 +1,19 @@
 def handleGET(path) :
+    # Map Requested Path
     path_dict = {
         "/" : "views/index.html",
         "/files.html" : "views/files.html"
     }
 
+    # Membuka file sesuai path
     try :
         file = open(path_dict[path], 'r')
     except (FileNotFoundError, KeyError) :
         file = open("views/404.html", 'r')
 
+    # Read File
     message_body = file.read()
     file.close()
 
+    # Return Read File
     return message_body
--- Tubes Jarkom/server.py
+++ Tubes Jarkom/server.py
@@ -6,26 +6,28 @@ from handler.requestHandler import *
 
 def threadingSocket(connectionSocket):
     try:
-        request = connectionSocket.recv(1024).decode()
-        response = handleRequest(request)
-        connectionSocket.send(response.encode())
-        connectionSocket.close()
+        request = connectionSocket.recv(1024).decode() #decode request
+        response = handleRequest(request) #handle request
+        connectionSocket.send(response.encode()) #kirimkan respon
+        connectionSocket.close() #tutup koneksi
     except IOError:
-        connectionSocket.send("File Not Found".encode())
-        connectionSocket.close()
+        connectionSocket.send("File Not Found".encode()) #file not found error
+        connectionSocket.close() #tutup koneksi
 
 if __name__ == "__main__":
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverAddress = "localhost"
-    serverPort = 80
+    serverSocket = socket(AF_INET, SOCK_STREAM) #inisiasi bahwa socket menggunakan IPv4, TCP
+    serverAddress = "localhost" #inisiasi nama server
+    serverPort = 80 #menentukan port server
 
-    # reuse port
+    #mengizinkan server untuk menggunakan socket dan alamat yang sama
+    #setelah koneksi sebelumnya berakhir 
     serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
-    serverSocket.bind((serverAddress, serverPort))
-    serverSocket.listen(5)
-    #prepare server socket
+    serverSocket.bind((serverAddress, serverPort)) #menyiapkan alamat server dan port pada socket
+    serverSocket.listen(5) #socket akan menunggu koneksi dan maksimal adalah 5 koneksi
+
+    #menampilkan server adress ke terminal
     print(f"\n\nYou can Acces Your Website in http://{serverAddress}:{serverPort}\n\n")
     while True:
-        connectionSocket, addr = serverSocket.accept()
-        threading.Thread(target=threadingSocket, args=(connectionSocket,)).start()
+        connectionSocket, addr = serverSocket.accept() #menerima permintaan koneksi
+        threading.Thread(target=threadingSocket, args=(connectionSocket,)).start() #memulai koneksi
